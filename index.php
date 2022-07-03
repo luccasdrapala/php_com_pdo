f7d84096b92201bf1f5415b37ac3c3f13dce9d01
luccasdrapala <luccasdrapala@gmail.com>
2022-07-03 16:00:49 -0300

    usando try catch para capturar possiveis PDOExceptios

--- index.php
+++ index.php
@@ -1,8 +1,15 @@
 <?php
 
-    $dsn = 'mysql:host=localhost;dbname=php_pdo';
+    $dsn = 'mysql:host=localhost;dbname=php_com_pdo';
     $usuario = 'root';
     $senha = '';
-    $conexao = new PDO($dsn, $usuario, $senha);
+
+    try {
+        $conexao = new PDO($dsn, $usuario, $senha);
+
+    } catch (PDOException $erro) {
+
+        echo 'Erro: ' . $erro->getCode() . ' Arquivo: ' . $erro->getFile() . ' Mensagem: ' . $erro->getMessage();
+    }
 
 ?>
