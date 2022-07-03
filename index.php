9c59d9ec52c8cd320eda03c3726d3f5f0eca3e7f
luccasdrapala <luccasdrapala@gmail.com>
2022-07-03 16:32:52 -0300

    usando metodo exec() do PDO para efetuar insert no banco

--- index.php
+++ index.php
@@ -4,8 +4,38 @@
     $usuario = 'root';
     $senha = '';
 
-    try {
+    echo 'opa, passei aqui';
+
+    try{
+
         $conexao = new PDO($dsn, $usuario, $senha);
+
+        $query = '
+            create table if not exists tb_usuarios(
+                id int not null primary key auto_increment,
+                nome varchar(50) not null,
+                email varchar(100) not null,
+                senha varchar(32) not null 
+            )
+        ';
+
+        $retorno = $conexao->exec($query);
+        echo $retorno;
+
+        $query = '
+            insert into tb_usuarios(
+                nome,
+                email,
+                senha
+            ) values (
+                "Luccas Drapala",
+                "luccasdrapala@gmail.com",
+                "senha123"
+            )    
+        ';
+
+        $retorno = $conexao->exec($query);
+        echo $retorno;
 
     } catch (PDOException $erro) {
 
