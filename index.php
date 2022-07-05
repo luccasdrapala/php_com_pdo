8c423878f50d785bba0b8bedac7e5f936e354392
luccasdrapala <luccasdrapala@gmail.com>
2022-07-05 20:59:31 -0300

    listando dados da query no foreach

--- index.php
+++ index.php
@@ -7,18 +7,25 @@
     try{
 
         $conexao = new PDO($dsn, $usuario, $senha);
-        $query = 'select * from tb_usuarios where id=6';
+        $query = 'select * from tb_usuarios';
 
         $statement = $conexao->query($query);
         print_r($statement);
         echo '<br>';
 
-        $lista = $statement->fetch(PDO::FETCH_OBJ);
+        $lista = $statement->fetchAll(PDO::FETCH_OBJ);//ou ASSOC
+
+        foreach($lista as $key => $value){
+            echo '<pre>';
+                print_r($value->nome);
+            echo '</pre><hr>';
+        }
+
+        /*
         echo '<pre>';
             print_r($lista);
         echo '</pre>';
-        echo $lista->nome;
-
+        echo $lista[0]->nome;*/
         //echo $lista[0]->nome . ' - ' . $lista[0]->email;
 
     } catch (PDOException $erro) {
