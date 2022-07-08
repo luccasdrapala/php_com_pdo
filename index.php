0a22466ee0ea33a11df99be46fb8e705c6c5ee1f
luccasdrapala <luccasdrapala@gmail.com>
2022-07-07 21:31:31 -0300

    usando prepare() e bindValue() para fazer a Query

--- index.php
+++ index.php
@@ -1,41 +1,47 @@
 <?php
 
-    $dsn = 'mysql:host=localhost;dbname=php_com_pdo';
-    $usuario = 'root';
-    $senha = '';
+    if(!empty($_POST['usuario']) && !empty($_POST['senha'])) {
 
-    try{
+        $dsn = 'mysql:host=localhost;dbname=php_com_pdo';
+        $usuario = 'root';
+        $senha = '';
 
-        $conexao = new PDO($dsn, $usuario, $senha);
-        $query = 'select * from tb_usuarios';
+        try{
 
-        foreach($conexao->query($query) as $key => $value){
-            echo '<pre>';
-                print_r($value['nome']);
-            echo '</pre><hr>';
+            $conexao = new PDO($dsn, $usuario, $senha);
+
+            $query = "select * from tb_usuarios where ";
+            $query .= "email = :usuario ";
+            $query .= " AND senha = :senha ";
+
+            $stmt = $conexao->prepare($query);
+
+            $stmt->bindValue(':usuario', $_POST['usuario']);
+            $stmt->bindValue(':senha', $_POST['senha']); //, PDO::PARAM_INT); extrai o conteudo como inteiro
+
+            $stmt->execute();
+            $usuario = $stmt->fetch(PDO::FETCH_OBJ);
+
+            print_r($usuario);
+
+        } catch (PDOException $erro) {
+
+            echo 'Erro: ' . $erro->getCode() . ' Arquivo: ' . $erro->getFile() . ' Mensagem: ' . $erro->getMessage();
         }
-
-        /*
-        $statement = $conexao->query($query);
-        print_r($statement);
-        echo '<br>';
-
-        $lista = $statement->fetchAll(PDO::FETCH_OBJ);//ou ASSOC
-
-        foreach($lista as $key => $value){
-            echo '<pre>';
-                print_r($value->nome);
-            echo '</pre><hr>';
-        }
-        echo '<pre>';
-            print_r($lista);
-        echo '</pre>';
-        echo $lista[0]->nome;*/
-        //echo $lista[0]->nome . ' - ' . $lista[0]->email;
-
-    } catch (PDOException $erro) {
-
-        echo 'Erro: ' . $erro->getCode() . ' Arquivo: ' . $erro->getFile() . ' Mensagem: ' . $erro->getMessage();
     }
 
 ?>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>login</title>
+    </head>
+    <body>
+        <form method="post" action="sql_injection.php">
+            <input type="text" placeholder="usuário" name="usuario"/><br><br>
+            <input type="password" placeholder="senha" name="senha"/><br><br>
+            <button type="submit">Login</button>
+        </form>
+    </body>
+</html>
